2c42953016c17916ae3d795c81daec82746cf472
abfm160418-droid <abfm160418@gmail.com>
2026-09-24 21:29:24 +0200

    Add system information script

--- sripts/system-info.sh
+++ sripts/system-info.sh
@@ -1,1 +1,19 @@
+#!/bin/bash
 
+# Linux System Information Script
+
+echo "===== SYSTEM INFORMATION ====="
+
+echo "Hostname:"
+hostname
+
+echo "Kernel version:"
+uname -r
+
+echo "System uptime:"
+uptime -p
+
+echo "Memory usage:"
+free -h
+
+echo "================================"
